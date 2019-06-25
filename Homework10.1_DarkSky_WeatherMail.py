9adbc25bb19d621976958b02868377c393805562
Yinan <dyn.acd@gmail.com>
2019-06-24 22:54:45 -0400

    Update Homework10.1_DarkSky_WeatherMail.py

--- Homework10.1_DarkSky_WeatherMail.py
+++ Homework10.1_DarkSky_WeatherMail.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[34]:
+# In[36]:
 
 
 import pandas as pd
@@ -9,7 +9,7 @@ import requests
 from bs4 import BeautifulSoup
 
 
-# In[35]:
+# In[37]:
 
 
 from dotenv import load_dotenv
@@ -17,7 +17,7 @@ load_dotenv()
 import os
 
 
-# In[36]:
+# In[38]:
 
 
 API_KEY = os.getenv("dark_sky_api_key")
@@ -25,23 +25,7 @@ url = f'https://api.darksky.net/forecast/{API_KEY}/37.8267,-122.4233'
 
 response = requests.get(url)
 data = response.json()
-
-
-# In[37]:
-
-
-temperature = data['currently']['temperature']
-summary = data['currently']['summary']
-today = data['daily']['data'][0]
-temp_feeling = 'warm'
-high_temp = today['temperatureHigh']
-low_temp = today['temperatureLow']
-
-
-# In[38]:
-
-
-print(f'Right now it is {temperature} degrees out and {summary}. Today will be {temp_feeling} with a high of {high_temp} and a low of {low_temp}. RAIN_WARNING.')
+data
 
 
 # In[ ]:
@@ -53,10 +37,34 @@ print(f'Right now it is {temperature} degrees out and {summary}. Today will be {
 # In[39]:
 
 
-import datetime
-right_now = datetime.datetime.now()
-date_string = right_now.strftime("%Y-%M")
-date_string
+from datetime import datetime
+timestamp = 1545730073
+dt_object = datetime.fromtimestamp(timestamp)
+print("dt_object =", dt_object)
+print("type(dt_object) =", type(dt_object))
+
+
+# In[40]:
+
+
+data.keys()
+
+
+# In[41]:
+
+
+temperature = data['currently']['temperature']
+summary = data['currently']['summary']
+today = data['daily']['data'][0]
+temp_feeling = 'warm'
+high_temp = today['temperatureHigh']
+low_temp = today['temperatureLow']
+
+
+# In[42]:
+
+
+print(f'Right now it is {temperature} degrees out and {summary}. Today will be {temp_feeling} with a high of {high_temp} and a low of {low_temp}. RAIN_WARNING.')
 
 
 # In[ ]:
@@ -65,15 +73,30 @@ date_string
 
 
 
-# In[40]:
+# In[43]:
+
+
+import datetime
+right_now = datetime.datetime.now()
+date_string = right_now.strftime("%B %d, %Y")
+date_string
+
+
+# In[45]:
+
+
+api_key = os.getenv('mailgun_api_key')
+
+
+# In[46]:
 
 
 response = requests.post("https://api.mailgun.net/v3/SANDBOX60ABF18F48944CD784D206060B22489E.MAILGUN.ORG/messages",
-                         auth=("api", "ca3ad75486548de3d51a9fed045a8cd1-29b7488f-0b33e891"),
+                         auth=("api", api_key),
                          data={"from": "DYN.ACD@GMAIL.COM",
                                "to": ["DYN.ACD@GMAIL.COM"],
-                               "subject": "Hello",
-                               "text": "Testing some Mailgun awesomness!"})
+                               "subject": "8AM Weather forecast:.",
+                               "text": "Good Morning! Here is today's weather forecast."})
 response.text
 
 
